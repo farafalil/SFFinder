0a7da9506bc4bd6997727798c27caef922219239
Hugo Anjos <hugo.h.anjos@hotmail.com>
2021-02-20 20:58:20 -0300

    adding the menubar icon/button on AppDelegate

--- SF Finder/AppDelegate.swift
+++ SF Finder/AppDelegate.swift
@@ -11,23 +11,19 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
+    var popover = NSPopover.init()
+    var statusBar: StatusBarController?
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.isReleasedWhenClosed = false
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        popover.contentSize = NSSize(width: 320, height: 360)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        
+        statusBar = StatusBarController.init(popover)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -36,4 +32,3 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 }
-
