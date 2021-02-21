0d8b6032b49e6a1ac29bb44c981fd0eb8943dc44
Hugo Anjos <hugo.h.anjos@hotmail.com>
2021-02-20 22:23:52 -0300

    button test

--- SF Finder/ContentView.swift
+++ SF Finder/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputSearch = ""
+    private var symbols = ["pencil", "scribble", "pencil.tip"]
+    
+    struct Symbol: View {
+        var name: String
+
+        var body: some View {
+            Button(action: {
+                let pasteboard = NSPasteboard.general
+                pasteboard.clearContents()
+                pasteboard.setString(name, forType: .string)
+            }) {
+                VStack {
+                    if #available(OSX 11.0, *) {
+                        Image(systemName: name)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                    Text(name)
+                }
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Section {
+                TextField("Search", text: $inputSearch)
+            }
+            .padding(.all, 10.0)
+            
+            Section {
+                ForEach(symbols, id: \.self) { symbol in
+                    Symbol(name: symbol)
+                }
+            }
+        }
     }
 }
 
